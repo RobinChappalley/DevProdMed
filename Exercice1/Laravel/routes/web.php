7848f369d4c3d251ea05c60d9c62029ece1bb334
RobinChappalley <robinchappalley@gmail.com>
2025-03-05 10:32:16 +0100

    Tried routes

--- Exercice1/Laravel/routes/web.php
+++ Exercice1/Laravel/routes/web.php
@@ -1,7 +1,55 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::get('1', function () {
+    return 'page 1';
 });
+Route::get('2', function () {
+    return 'page 2';
+});
+Route::get('3', function () {
+    return 'page 3';
+});
+Route::get('4', function () {
+    return 'page 5';
+});
+
+Route::get(' {n}', function ($n) {
+    return "page $n";
+})->where('n', '[1-3]');
+
+Route::get('afficheDate', function () {
+    $date = now();
+    echo 'Voici le détail de la variable date :' . '<BR>';
+    var_dump($date);
+
+    echo '<BR>';
+    echo "Aujourd'hui nous sommes le " . $date;
+});
+
+
+Route::get('afficheDate2', function () {
+    $date = now();
+    echo 'Voici le détail de la variable date :' . '<BR>';
+    dd($date);
+
+});
+Route::get('cff', function () {
+    return redirect('https://www.sbb.ch/fr/');
+});
+Route::get('maison', function () {
+    return redirect()->route('home');
+});
+
+Route::get(
+    'uneRoute/{param1}/{param2}/{param3?}',
+    function ($param1, $param2, $param3 = "Laravel") {
+        return $param1 . ' ' . $param2 . ' ' . $param3;
+    }
+);
