f9b2d14189c4024947139808331184fab5e33380
RobinChappalley <robinchappalley@gmail.com>
2025-03-08 18:43:22 +0100

    Create dark mode

--- Exercice2/Laravel/resources/views/monTemplate.blade.php
+++ Exercice2/Laravel/resources/views/monTemplate.blade.php
@@ -5,6 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <style>
+        body {
+            background-color:rgb(39, 37, 37);
+        }
+        p {
+            color: white;
+        }
+    </style>
 </head>
 
 <body>
