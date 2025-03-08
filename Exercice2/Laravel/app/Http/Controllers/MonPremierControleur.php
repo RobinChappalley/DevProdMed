5a0ee6f98b4e3ac1c7f17bb77e79a7a02cf77501
RobinChappalley <robinchappalley@gmail.com>
2025-03-08 17:32:01 +0100

    Implement logic in the controller to display an array of artists

--- Exercice2/Laravel/app/Http/Controllers/MonPremierControleur.php
+++ Exercice2/Laravel/app/Http/Controllers/MonPremierControleur.php
@@ -3,11 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DateTime;
 class MonPremierControleur extends Controller
 {
     public function maMethodeDansControleur()
     {
         return view('welcome');
+    }
+
+    public function test($n, $c)
+    {
+        return $n . " : " . $c;
+    }
+
+    public function afficherTableau()
+    {
+        $artistes = array(
+            array(
+                "nom" => "Amy",
+                "prenom" => "Winehouse",
+                "dateNaissance" => new DateTime('14-09-1983')
+            ),
+            array(
+                "nom" => "Janis",
+                "prenom" => "Joplin",
+                "dateNaissance" => new DateTime('19-01-1943')
+            ),
+            array(
+                "nom" => "Jo",
+                "prenom" => "Bar",
+                "dateNaissance" => new DateTime('19-01-1943')
+            ),
+            array(
+                "nom" => "Janis",
+                "prenom" => "Siegel",
+                "dateNaissance" => new DateTime('12-01-1990')
+            ),
+        );
+        return view('maVue2')->with('artistes', $artistes);
     }
 }
--- Exercice2/Laravel/routes/web.php
+++ Exercice2/Laravel/routes/web.php
@@ -8,3 +8,8 @@ Route::get('/', [MonPremierControleur::class, 'maMethodeDansControleur']);
 Route::get('article/{n}/couleur/{c}', function ($n, $c) {
     return view('maVue')->with('numero', $n)->with('couleur', $c);
 })->where(['n' => '[0-9]+', 'c' => 'rouge|vert|bleu']);
+
+Route::get('test/{n}/{c}', [MonPremierControleur::class, 'test'])
+    ->where(['n' => '[0-9]+', 'c' => 'rouge|vert|bleu']);
+
+Route::get('afficherTableau' , [MonPremierControleur::class, 'afficherTableau']);
