4d0016dd5ececac3d7e73ef36338ff1b8a2af640
RobinChappalley <robinchappalley@gmail.com>
2025-03-08 23:01:56 +0100

    Create function to get proverbs directly from the wiki page

--- Exercice2/Laravel/app/Http/Controllers/controleurDeProverbes.php
+++ Exercice2/Laravel/app/Http/Controllers/controleurDeProverbes.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Dom\HTMLDocument;
+use DOMDocument;
 use Illuminate\Http\Request;
 
 class controleurDeProverbes extends Controller
@@ -10,12 +12,14 @@ class controleurDeProverbes extends Controller
     public function afficherProverbes()
     {
 
-        $proverbes = $this->recupererProverbes();
+        $proverbes = $this->recupererProverbesDepuisLeWeb();
         for ($i = 0; $i < self::NBPROVERBESVOULUS; $i++) {
             $nProverbe = rand(0, count($proverbes) - 1);
-            $dixProverbes[] = $proverbes[$nProverbe];   
+            $dixProverbes[] = $proverbes[$nProverbe];
         }
         return view('proverbes', ['proverbes' => $dixProverbes]);
+
+
     }
     public function recupererProverbes()
     {
@@ -29,5 +33,21 @@ class controleurDeProverbes extends Controller
         // var_dump($proverbes);
         return $proverbes;
 
+    }
+
+    public function recupererProverbesDepuisLeWeb()
+    {
+
+        //Cela fonction car on masque les erreurs HTML !
+        $file = new DOMDocument();
+        $html = file_get_contents("https://fr.wiktionary.org/wiki/Annexe:Liste_de_proverbes_fran%C3%A7ais");
+        $file->loadHTML($html, LIBXML_NOERROR);
+        $proverbes = [];
+        $elements = $file->getElementsByTagName("li");
+        foreach ($elements as $element) {
+            $proverbes[] = $element->textContent;
+        }
+
+        return $proverbes;
     }
 }
