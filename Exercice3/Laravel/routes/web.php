82cfa8f97f34796cf99bcf817c2ed787f2be076d
RobinChappalley <robinchappalley@gmail.com>
2025-03-10 14:09:02 +0100

    Adding routes

--- Exercice3/Laravel/routes/web.php
+++ Exercice3/Laravel/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/form', function () {
+    return view('view_form');
+});
+
+Route::post('/traiteFormulaire', function () {
+    return view('view_form');
+});
