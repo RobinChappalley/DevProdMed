8467f0dd2fe7a04d043f98794e27335c56c7868f
RobinChappalley <robinchappalley@gmail.com>
2025-03-30 17:40:05 +0200

    Adapt view to display the infos about meetings

--- Exercice3/Laravel/resources/views/view_reception.blade.php
+++ Exercice3/Laravel/resources/views/view_reception.blade.php
@@ -1,14 +1,11 @@
 @extends('form_template')
 @section('contenu')
     <p>Voici votre planning de rendez-vous de la journée ! :</p>
+    <p>Horaires des rendez-vous :</p>
     <ul>
-        @foreach ($eleves as $eleve)
-            <li>{{ $eleve }} :
-                @foreach ($tableauheures as $horaire)
-                    {{ $horaire[0]}} <br> - {{ $horaire[1]}}
-                    <br>
-                @endforeach
-            </li>
-        @endforeach
+        @for ($i = 0; $i < count($eleves); $i++)
+            <li>{{ $eleves[$i] }} : {{ $tableauheures[$i][0] }} - {{ $tableauheures[$i][1] }}</li>
+        @endfor
     </ul>
+
 @endsection
