c9a4225a0b1cd2a16720cfbc26ed9fc863888c40
RobinChappalley <robinchappalley@gmail.com>
2025-04-03 15:46:54 +0200

    Update languages

--- Exercice4/routes/web.php
+++ Exercice4/routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ContactController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+    
 
 Route::get('/contact', [ContactController::class, 'rendFormulaire']);
 Route::post('/contact', [ContactController::class, 'valideEtTraiteFormulaire']);
