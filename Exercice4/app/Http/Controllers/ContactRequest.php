7a81282ba70b49b6936af78cca5b00ba733c3845
RobinChappalley <robinchappalley@gmail.com>
2025-04-03 14:44:15 +0200

    Create validation class to define the necesarry rules for the form to be valid

---
+++ Exercice4/app/Http/Controllers/ContactRequest.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+class ContactRequest extends FormRequest
+{
+    /**
+     * Détermine si l'utilisateur est autorisé à faire cette requête.
+     */
+    public function authorize(): bool
+    {
+        return false; // Par défaut, l'accès est refusé.
+    }
+    /**
+     * Définit les règles de validation des champs du formulaire.
+     */
+    public function rules(): array
+    {
+        return [
+            'nom' => 'bail|required|min:3|max:20|alpha',
+            'email' => 'required|email',
+            'texte' => 'required|max:250'
+        ];
+// · bail : Arrête la validation dès la première erreur pour un champ.
+// • required : Le champ est obligatoire.
+// • min:3 : Le champ nom doit contenir au moins 3 caractères.
+// • max:20 : Le champ nom ne peut pas dépasser 20 caractères.
+// • alpha : Le champ nom ne doit contenir que des lettres.
+// • email : Vérifie que le champ email contient une adresse valide.
+// • max:250 : Le champ texte est limité à 250 caractères.
+    }
+}
