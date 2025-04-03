90b0e33b1078f1200350dac0e15705e2a6f9d378
RobinChappalley <robinchappalley@gmail.com>
2025-04-03 15:46:30 +0200

    Renamed views with blade.php

---
+++ Exercice4/resources/views/view_confirmation.blade.php
@@ -0,0 +1,10 @@
+@extends('template_contact')
+@section('contenu')
+    <div class="col-sm-offset-3 col-sm-6"></div>
+    <div class="panel panel-info">
+        <div class="panel-heading">Information</div>
+        <div class="panel-body">Merci, vos informations ont bien été
+            transmises à l'administrateur.</div>
+    </div>
+    </div>
+@endsection('contenu')
